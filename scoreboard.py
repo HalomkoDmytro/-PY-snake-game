08c25fd57be3b7cc4a66138d5fa7f82622616a21
HalomkoDmytro <galomko.dmytro@gmail.com>
2022-09-11 18:04:06 +0300

    add save to file score

--- scoreboard.py
+++ scoreboard.py
@@ -6,19 +6,25 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.update_scoreboard()
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align="center", font=("Arial", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
